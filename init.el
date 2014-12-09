c274e5c0e9c6cc13c379cede2b73edc8d88d90e5
wizjin <wizjin@users.noreply.github.com>
2014-12-09 16:34:34 +0800

    Config EShell.

--- init.el
+++ init.el
@@ -5,18 +5,21 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "site-lisp" user-emacs-directory))
 
+;; Color Theme
+(add-to-list 'custom-theme-load-path
+			 (expand-file-name "themes" user-emacs-directory))
+(load-theme 'tomorrow-night-paradise t)
+(setq color-theme-is-global t)
+
 ;; Package
 (require 'package)
 (package-initialize)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
-;; Color Theme
-(require 'tomorrow-night-paradise-theme)
-(setq color-theme-is-global t)
-
 ;; Init
 (require 'init-misc)
 (require 'init-elpa)
+(require 'init-eshell)
 
 (provide 'init)
