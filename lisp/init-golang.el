a09e01f82ef0a524b20a109bd703d5cdbf2525ae
wizjin <wizjin@users.noreply.github.com>
2014-12-12 13:38:13 +0800

    Update golfing for version 1.4.

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -1,5 +1,21 @@
-(require 'go-mode-load)
+(require-package 'go-mode)
+(require 'go-mode-autoloads)
+
+(require-package 'go-autocomplete)
 (require 'go-autocomplete)
+
+(defun go/run ()
+  "Run current buffer"
+  (interactive)
+  (shell-command (concat "go run \"" (buffer-file-name) "\"")))
+
 (add-hook 'before-save-hook 'gofmt-before-save)
+(add-hook 'go-mode-hook (lambda () (local-set-key (kbd "<f5>") 'go/run)))
+(add-hook 'go-mode-hook (lambda () (local-set-key (kbd "M-.") 'godef-jump)))
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-c C-g") 'go-goto-imports)))
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-c C-r")
+                                         'go-remove-unused-imports)))
 
 (provide 'init-golang)
