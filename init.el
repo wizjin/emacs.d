9fc817d60269ed77233080524d6926a5bc435ad9
wizjin <wizjin@users.noreply.github.com>
2017-11-06 12:07:37 +0800

    Add program language support

--- init.el
+++ init.el
@@ -13,10 +13,10 @@
 ;; Adjust garbage collection thresholds during startup, and thereafter
 ;;----------------------------------------------------------------------------
 (let ((normal-gc-cons-threshold (* 20 1024 1024))
-	  (init-gc-cons-threshold (* 128 1024 1024)))
+          (init-gc-cons-threshold (* 128 1024 1024)))
   (setq gc-cons-threshold init-gc-cons-threshold)
   (add-hook 'after-init-hook
-			(lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
 ;;----------------------------------------------------------------------------
 ;; Bootstrap config
@@ -65,9 +65,43 @@
 (require 'init-fonts)
 (require 'init-mmm)
 
-;(require 'init-editing-utils)
+(require 'init-editing-utils)
 (require 'init-whitespace)
 
+
+(require 'init-git)
+
+(require 'init-projectile)
+
+(require 'init-compile)
+(require 'init-textile)
+(require 'init-markdown)
+(require 'init-csv)
+(require 'init-javascript)
+(require 'init-html)
+(require 'init-css)
+(require 'init-python-mode)
+(require 'init-yaml)
+(require 'init-docker)
+(maybe-require-package 'terraform-mode)
+
+
+(require 'init-lisp)
+(require 'init-common-lisp)
+
+(when *spell-check-support-enabled*
+  (require 'init-spelling))
+
+
+(when *is-a-mac*
+  (require-package 'osx-location))
+(maybe-require-package 'regex-tool)
+
+;;----------------------------------------------------------------------------
+;; Variables configured via the interactive 'customize' interface
+;;----------------------------------------------------------------------------
+(when (file-exists-p custom-file)
+  (load custom-file))
 
 
 
